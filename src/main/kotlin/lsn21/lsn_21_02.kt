a7800a7cf348fcb84db061b7bb1b31ef0f4ece6e
QiuGong <178438297@qq.com>
2022-11-23 22:35:49 +0800

    lsn 21

--- src/main/kotlin/lsn21/lsn_21_02.kt
+++ src/main/kotlin/lsn21/lsn_21_02.kt
@@ -1,5 +1,7 @@
 package lsn21
 
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.channels.produce
@@ -106,7 +108,7 @@ Time cost: 540
 
 
 /////////////////////////////////////////////////////////////////////////////
-fun main() = runBlocking {
+fun main023() = runBlocking {
     val startTime = System.currentTimeMillis()
     val channel1 = produce<String> {
         // 变化在这里
@@ -145,4 +147,188 @@ a
 b
 c
 Time cost: 533
+*/
+
+
+/////////////////////////////////////////////////////////////////////////////
+fun main024() = runBlocking {
+    val startTime = System.currentTimeMillis()
+    val channel1 = produce<String> {
+        // 变化在这里
+        delay(15000L)
+    }
+
+    val channel2 = produce {
+        delay(100L)
+        send("a")
+        delay(200L)
+        send("b")
+        delay(200L)
+        send("c")
+    }
+
+    suspend fun selectChannel(channel1: ReceiveChannel<String>, channel2: ReceiveChannel<String>): String = select<String> {
+        channel1.onReceive{
+            it.also { println(it) }
+        }
+        channel2.onReceive{
+            it.also { println(it) }
+        }
+    }
+
+    // 变化在这里
+    repeat(6){
+        selectChannel(channel1, channel2)
+    }
+
+    println("Time cost: ${System.currentTimeMillis() - startTime}")
+}
+
+/*
+输出结果
+a
+b
+c
+Time cost: 533
+Exception in thread "main" kotlinx.coroutines.channels.ClosedReceiveChannelException
+*/
+
+
+/////////////////////////////////////////////////////////////////////////////
+fun main025() = runBlocking {
+    val startTime = System.currentTimeMillis()
+    val channel1 = produce<String> {
+        delay(15000L)
+    }
+
+    val channel2 = produce {
+        delay(100L)
+        send("a")
+        delay(200L)
+        send("b")
+        delay(200L)
+        send("c")
+    }
+
+    suspend fun selectChannel(channel1: ReceiveChannel<String>, channel2: ReceiveChannel<String>): String =
+        select<String> {
+            channel1.onReceiveCatching {
+                it.getOrNull() ?: "channel1 is closed!"
+            }
+            channel2.onReceiveCatching {
+                it.getOrNull() ?: "channel2 is closed!"
+            }
+        }
+
+    repeat(6) {
+        val result = selectChannel(channel1, channel2)
+        println(result)
+    }
+
+    println("Time cost: ${System.currentTimeMillis() - startTime}")
+}
+
+/*
+输出结果
+a
+b
+c
+channel2 is closed!
+channel2 is closed!
+channel2 is closed!
+Time cost: 541
+程序不会立即退出
+*/
+
+
+/////////////////////////////////////////////////////////////////////////////
+fun main026() = runBlocking {
+    val startTime = System.currentTimeMillis()
+    val channel1 = produce<String> {
+        delay(15000L)
+    }
+
+    val channel2 = produce {
+        delay(100L)
+        send("a")
+        delay(200L)
+        send("b")
+        delay(200L)
+        send("c")
+    }
+
+    suspend fun selectChannel(channel1: ReceiveChannel<String>, channel2: ReceiveChannel<String>): String =
+        select<String> {
+            channel1.onReceiveCatching {
+                it.getOrNull() ?: "channel1 is closed!"
+            }
+            channel2.onReceiveCatching {
+                it.getOrNull() ?: "channel2 is closed!"
+            }
+        }
+
+    repeat(6) {
+        val result = selectChannel(channel1, channel2)
+        println(result)
+    }
+
+    // 变化在这里
+    channel1.cancel()
+    channel2.cancel()
+
+    println("Time cost: ${System.currentTimeMillis() - startTime}")
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+fun main() = runBlocking {
+    suspend fun <T> fastest(vararg deferreds: Deferred<T>): T = select {
+        fun cancelAll() = deferreds.forEach { it.cancel() }
+
+        for (deferred in deferreds) {
+            deferred.onAwait {
+                cancelAll()
+                it
+            }
+        }
+    }
+
+    val deferred1 = async {
+        delay(100L)
+        println("done1")    // 没机会执行
+        9.8f
+    }
+
+    val deferred2 = async {
+        delay(50L)
+        println("done2")
+        8888
+    }
+
+    val deferred3 = async {
+        delay(10000L)
+        println("done3")    // 没机会执行
+        "result3"
+    }
+
+    val deferred4 = async {
+        delay(2000L)
+        println("done4")    // 没机会执行
+        1L
+    }
+
+    val deferred5 = async {
+        delay(14000L)
+        println("done5")    // 没机会执行
+        Any()
+    }
+
+    val result = fastest(deferred1, deferred2, deferred3, deferred4, deferred5)
+    println(result)
+}
+
+/*
+输出结果
+done2
+result2
 */
